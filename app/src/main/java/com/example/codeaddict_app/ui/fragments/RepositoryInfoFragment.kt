9381c0ab5c94349c4f067588c4afaebfda33af5c
Wiktoria Tarnowska <wikktamm@gmail.com>
2020-12-06 10:05:30 +0100

    #R6 Add divider line in commits rv

--- app/src/main/java/com/example/codeaddict_app/ui/fragments/RepositoryInfoFragment.kt
+++ app/src/main/java/com/example/codeaddict_app/ui/fragments/RepositoryInfoFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.codeaddict_app.R
@@ -72,6 +73,7 @@ class RepositoryInfoFragment : BaseFragment(R.layout.fragment_repository_info) {
         with(rv_commits) {
             adapter = commitsAdapter
             layoutManager = LinearLayoutManager(requireContext())
+            addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
         }
     }
 
